84149dec5b7556678cc087eb06aa5ce86e32d1f5
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-08-23 05:02:56 +0100

    commit

--- src/app/account/my-library/layout.tsx
+++ src/app/account/my-library/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import TabNav from "@/components/header/TabNav";
+import { useLibraryStore } from "@/store";
 import React, { ReactNode, useState } from "react";
 
 type Props = {
@@ -8,9 +9,12 @@ type Props = {
 
 const Layout = (props: Props) => {
   const [tab, SetTab] = useState("all");
+  const library = useLibraryStore();
   const handleTabChange = (text: string) => {
     SetTab(text);
+    library.inc();
   };
+  console.log({ count: library.count });
   return (
     <div className="">
       <div className="" id="tab">
