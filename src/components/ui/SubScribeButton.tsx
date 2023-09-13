9ad45d030f6e5b097aa068dbc7bf2403b71913e2
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-09-13 13:10:56 +0100

    commit

--- src/components/ui/SubScribeButton.tsx
+++ src/components/ui/SubScribeButton.tsx
@@ -14,42 +14,23 @@ const SubScribeButton = (props: Props) => {
   const session = useSession();
   const router = useRouter();
 
-  const { user } = session.data;
-  //   console.log({ user });
-  //   const config = {
-  //     reference: new Date().getTime().toString(),
-  //     email: user.email,
-  //     plan: code,
-  //     amount: amount * 100, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
-  //     publicKey: "pk_test_3ed7db366f045a0f95b48fb545a11a0c7f1b5456",
-  //   };
-  //   const initializePayment = usePaystackPayment(config);
-  //   const onSuccess = (reference) => {
-  //     // Implementation for whatever you want to do with reference and after success call.
-  //     console.log({ reference });
-  //     alert("Thanks for doing business with us! Come back soon!!");
-  //   };
-  //   const onClose = () => {
-  //     // implementation for  whatever you want to do when the Paystack dialog closed.
-  //     console.log("closed");
-  //     alert("Wait! You need this oil, don't go!!!!");
-  //   };
-
   const Subscribe = async () => {
-    const { data: resData } = await axios.post("/api/payment", {
-      email: user.email,
-      amount,
-      code,
-    });
-    const { data } = resData;
-    if (data) {
-      router.push(`${data.authorization_url}`);
-      console.log({ data });
-      console.log(data.authorization_url);
-      //   window.location.href = data.authorization_url;
+    console.log({ session });
+    if (!session.data) {
+      router.push("/login");
+    } else {
+      const user = session?.data.user;
+      const { data: resData } = await axios.post("/api/payment", {
+        email: user.email,
+        amount,
+        code,
+      });
+      const { data } = resData;
+      if (data) {
+        router.push(`${data.authorization_url}`);
+        console.log(data.authorization_url);
+      }
     }
-
-    // console.log({ data });
   };
 
   return (
